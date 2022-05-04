72a9e33b704a6a8b22a05a1de5d408066e7455c3
tristanfazio <tristan.fazio@outlook.com.au>
2022-05-05 02:31:06 +0800

    about me updates

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -26,8 +26,13 @@ const About = () => {
 			<path d="M480 288H32c-17.62 0-32 14.38-32 32v128c0 17.62 14.38 32 32 32h448c17.62 0 32-14.38 32-32v-128C512 302.4 497.6 288 480 288zM352 408c-13.25 0-24-10.75-24-24s10.75-24 24-24s24 10.75 24 24S365.3 408 352 408zM416 408c-13.25 0-24-10.75-24-24s10.75-24 24-24s24 10.75 24 24S429.3 408 416 408zM480 32H32C14.38 32 0 46.38 0 64v128c0 17.62 14.38 32 32 32h448c17.62 0 32-14.38 32-32V64C512 46.38 497.6 32 480 32zM352 152c-13.25 0-24-10.75-24-24S338.8 104 352 104S376 114.8 376 128S365.3 152 352 152zM416 152c-13.25 0-24-10.75-24-24S402.8 104 416 104S440 114.8 440 128S429.3 152 416 152z" />
 		</svg>
 	);
+	const chevron = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+			<path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
+		</svg>
+	);
 	return (
-		<div id={"about"} className={styles.about}>
+		<div id={"about"} className={styles.aboutSection}>
 			<h1>
 				<span className={`${styles.flare} ${styles.number}`}>2.</span>{" "}
 				About Me
@@ -36,8 +41,10 @@ const About = () => {
 				<div className={styles.aboutMe}>
 					<h2>Me in a nutshell</h2>
 					<p>
-						Hi I'm Tristan and I love working on web apps. I've
-						always been drawn to coding. Starting with niche
+						<span className={styles.flare}>
+							Hi I'm Tristan and I love working on web apps.
+						</span>{" "}
+						I've always been drawn to coding. Starting with niche
 						interests like butchering custom MySpace layouts to
 						tinkering with game mods, tackling a problem was always
 						satisfying.
@@ -63,7 +70,12 @@ const About = () => {
 					</p>
 				</div>
 				<div className={styles.careerHighlights}>
-					<h2>Some Career Highlights</h2>
+					<h2>Some career highlights</h2>
+					<ul>
+						<li>{chevron}</li>
+						<li>{chevron}</li>
+						<li>{chevron}</li>
+					</ul>
 				</div>
 				<div className={styles.dotPointArea}>
 					<div>
